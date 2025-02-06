1422fd0b2c5f7882f229bd25ae4a6f6300360a95
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-06 15:35:41 +0100

    complete navbar design

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
-<script>
+<script setup>
   // import CitiesList from "./components/CitiesList.vue";
   // import HelloWorld from "./components/HelloWorld.vue";
   import FetchPB from "./components/FetchPB.vue";
+  import Navbar from "./components/Navbar.vue";
 
-  export default {
-    components: {
-      FetchPB,
-    }
-  };
 </script>
 
 <template>
-  <FetchPB  msg="Jomen tjena!" />
+  <Navbar></Navbar>
+  <!-- <FetchPB  msg="Jomen tjena!" /> -->
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <span id="logo">S4F</span>
+    <span id="items">
+      <span id="cart" class="icon"><div id="dot"></div><img src="../../assets/cart.svg" alt=""></span>
+      <span id="burger" class="icon"><img src="../../assets/burger.svg" alt=""></span>
+
+    </span>
+  </nav>
+</template>
+
+<style socped>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 5em;
+    width: 100vw;
+    background-color: var(--dark);
+  }
+  span {
+    display: flex;
+    justify-content: center;
+    padding: 0 1.5rem;
+    font-size: 2em;
+    font-weight: bold;
+    color: var(--light);
+    font-family: Montserrat;
+  }
+  #dot {
+    /* Appears if cart contains items, display: block = on, none = off */
+    display: block;
+    position: absolute;
+    height: 13px;
+    width: 13px;
+    left: 39px;
+    bottom: 29px;
+    background-color: var(--red);
+    border: var(--dark) 8px solid ;
+    border-radius: 100px;
+  }
+
+  #cart {
+    position: relative;
+    transform: scale(.6);
+  }
+  #burger {
+    transform: scale(.5);
+  }
+
+  .icon {
+    padding: 0 .5rem;
+    /* border:solid var(--red); */
+  }
+</style>
