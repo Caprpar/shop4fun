11edcf36a2b31663dfb5b10d6f7b9761d9521616
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-10 09:26:08 +0100

    add pages to views folder

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from "vue";
 import "../assets/main.css";
 
 import App from "./App.vue";
-import router from "./router.js";
+import router from "./router";
 
 createApp(App).use(router).mount("#app");
---
+++ src/views/ContactsView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hej kontakt</h1>
+</template>
